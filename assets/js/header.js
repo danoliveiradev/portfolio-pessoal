5918b6631025d74e73c9becf5b96e052502fe4f1
danoliveiradev <danielroliveira.dev@gmail.com>
2023-01-13 00:21:16 -0300

    alteração de cores, add efeitos

--- assets/js/header.js
+++ assets/js/header.js
@@ -10,27 +10,34 @@ const linkSecao2 = document.querySelector('.secao2');
 const linkSecao3 = document.querySelector('.secao3');
 const linkSecao4 = document.querySelector('.secao4');
 
+
+// Evento de click menu hamburguer
 btnMenu.addEventListener('click', () => {
-  exibeMenuLateral();
+  if (menuMobile.style.display === 'none') {
+    exibeMenuLateral();
+  } else {
+    removeMenuLateral();
+  }
 })
 
-// Abre e fecha o menu hamburguer
+// Função que exibe o menu lateral
 function exibeMenuLateral() {
-  if (menuMobile.style.display === 'none') {
-    menuMobile.style.display = 'block';
-    btnMenu.classList.replace('menu-hamburguer--abrir', 'menu-hamburguer--fechar');
-    efeitoGlass.classList.add('menu--efeito-glass');
-  } else {
-      removeMenuLateral();
-  }
+  menuMobile.style.display = 'block';
+  btnMenu.classList.replace('menu-hamburguer--abrir', 'menu-hamburguer--fechar');
+  efeitoGlass.classList.add('menu--efeito-glass');
+  sombraAtiva()
 }
 
+// Função que remove o menu lateral
 function removeMenuLateral() {
   menuMobile.style.display = 'none';
   btnMenu.classList.replace('menu-hamburguer--fechar', 'menu-hamburguer--abrir');
   efeitoGlass.classList.remove('menu--efeito-glass');
+  sombraDesativada()
 }
 
+
+// Eventos de click da seção
 linkSecao1.addEventListener('click', () => {
   removeMenuLateral();
 })
@@ -43,3 +50,26 @@ linkSecao3.addEventListener('click', () => {
 linkSecao4.addEventListener('click', () => {
   removeMenuLateral();
 })
+
+// ---------- Cabecalho muda de cor ao rolar tela ----------
+
+let rolar = document.documentElement;
+const cabecalho = document.querySelector('.cabecalho');
+
+window.addEventListener('scroll', () => {
+  if (rolar.scrollTop > 0) {
+    sombraAtiva()
+  } else {
+    sombraDesativada()
+  }
+})
+
+// Função ativa sombra do cabeçalho
+function sombraAtiva() {
+  cabecalho.style.boxShadow = '0px 0px 8px 0px #212121';
+}
+
+// Função desativa sombra do cabeçalho
+function sombraDesativada() {
+  cabecalho.style.boxShadow = 'none';
+}
