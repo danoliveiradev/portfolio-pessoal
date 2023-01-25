26c32ea37c5c66243a9163a16ef1513a5a988d93
danoliveiradev <danielroliveira.dev@gmail.com>
2023-01-24 21:49:14 -0300

    ajuste funcao rolar tela, carrosel infinito

--- assets/js/carrosel.js
+++ assets/js/carrosel.js
@@ -5,21 +5,66 @@ const btnDirPub = document.querySelector('.btn_dir-pub');
 const carroselProj = document.querySelector('.carrosel-proj');
 const carroselPub = document.querySelector('.carrosel-pub');
 
+const valorRolar = 1000;
+var valorScroll = 0;
+
 // Movimenta o carrosel da secao Projetos
 btnEsqProj.addEventListener('click', () => {
-  carroselProj.scrollLeft -= 200;
+  valorScroll = parseInt(carroselProj.scrollLeft);
+  rolarTela('esqProj', valorScroll);
 });
 
 btnDirProj.addEventListener('click', () => {
-  carroselProj.scrollLeft += 200;
+  valorScroll = parseInt(carroselProj.scrollLeft);
+  rolarTela('dirProj', valorScroll);
 });
-
 
 // Movimenta o carrosel da secao Publicacoes
 btnEsqPub.addEventListener('click', () => {
-  carroselPub.scrollLeft -= 200;
+  valorScroll = parseInt(carroselPub.scrollLeft);
+  rolarTela('esqPub', valorScroll);
 });
 
 btnDirPub.addEventListener('click', () => {
-  carroselPub.scrollLeft += 200;
+  valorScroll = parseInt(carroselPub.scrollLeft);
+  rolarTela('dirPub', valorScroll);
 });
+
+function rolarTela(direcao, valor) {
+  var TelaMin = carroselProj.clientWidth;
+  var TelaMax = carroselProj.scrollWidth;
+
+  switch (direcao) {
+    case 'esqProj':
+      if (valor === 0) {
+        carroselProj.scrollLeft = (TelaMax - TelaMin + 1);
+      } else {
+        carroselProj.scrollLeft -= valorRolar;
+      }
+      break;
+
+    case 'dirProj':
+      if (valor === (TelaMax - TelaMin + 1)) {
+        carroselProj.scrollLeft = 0;
+      } else {
+        carroselProj.scrollLeft += valorRolar;
+      }
+      break;
+
+    case 'esqPub':
+      if (valor === 0) {
+        carroselPub.scrollLeft = (TelaMax - TelaMin + 1);
+      } else {
+        carroselPub.scrollLeft -= valorRolar;
+      }
+      break;
+
+    case 'dirPub':
+      if (valor === (TelaMax - TelaMin + 1)) {
+        carroselPub.scrollLeft = 0;
+      } else {
+        carroselPub.scrollLeft += valorRolar;
+      }
+      break;
+  }
+}
